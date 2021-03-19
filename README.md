29ef8a262f66b1b840a955727ed96c7d43896682
Tibor Bödecs <mail.tib@gmail.com>
2021-03-19 08:51:11 +0100

    Tests, license, readme update

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
-# hook-kit
+# HookKit
 
-A description of this package.
+Generic event hook functions and invocation for Swift.
+
+
+## Install
+
+Add the repository as a dependency:
+
+```swift
+.package(url: "https://github.com/binarybirds/hook-kit.git", from: "1.0.0"),
+```
+
+Add HookKit to the target dependencies:
+
+```swift
+.product(name: "HookKit", package: "hook-kit"),
+```
+
+Update the packages and you are ready to use HookKit.
+
+
+## Basic usage
+
+```swift
+let hooks = HookStorage()
+hooks.register("test") { _ in "Hello world" }
+let result: String? = hooks.invoke("test")
+print(result) /// Optional("Hello world")
+```
+
+To learn more about hook functions, please read my [article](https://theswiftdev.com/event-driven-generic-hooks-for-swift/).
--- Tests/HookKitTests/HookKitTests.swift
+++ Tests/HookKitTests/HookKitTests.swift
@@ -3,7 +3,42 @@ import XCTest
 
 final class HookKitTests: XCTestCase {
 
-    func testExample() {
-        XCTAssertTrue(true)
+    func testSingleInvocation() {
+        let hooks = HookStorage()
+        hooks.register("test") { _ in "Hello world" }
+        let result: String? = hooks.invoke("test")
+        XCTAssertEqual(result, "Hello world")
+    }
+    
+    func testSingleTypedInvocation() {
+        let hooks = HookStorage()
+        hooks.register("test") { _ in 123 }
+        hooks.register("test") { _ in "Hello world" }
+        let result: String? = hooks.invoke("test")
+        XCTAssertEqual(result, "Hello world")
+    }
+    
+    func testSingleArguments() {
+        let hooks = HookStorage()
+        hooks.register("test") { args in args["foo"] as! String }
+        let result: String? = hooks.invoke("test", args: ["foo": "bar"])
+        XCTAssertEqual(result, "bar")
+    }
+
+    func testMultiInvocation() {
+        let hooks = HookStorage()
+        hooks.register("test") { _ in 1 }
+        hooks.register("test") { _ in 2 }
+        let result: [Int] = hooks.invokeAll("test")
+        XCTAssertEqual(result, [1, 2])
+    }
+    
+    func testMultiTypedInvocation() {
+        let hooks = HookStorage()
+        hooks.register("test") { _ in 1 }
+        hooks.register("test") { _ in 2 }
+        hooks.register("test") { _ in "Hello world" }
+        let result: [Int] = hooks.invokeAll("test")
+        XCTAssertEqual(result, [1, 2])
     }
 }
